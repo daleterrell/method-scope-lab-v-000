9f48cdfd8cd7a39530184c204dc1c87c3b63c5fd
Dale Terrell <tragicid@gmail.com>
2018-05-02 04:48:56 +0000

    automatically backed up by learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,12 @@
-def phrase(catch_phrase)
-  puts phrase
+#def phrase(catch_phrase)
+ # puts phrase
+#end
+
+
+require "spec_helper"
+describe "#catch_phrase" do 
+  it "puts out a catch phrase" do 
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
